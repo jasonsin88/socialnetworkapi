b082e0cd15a8b386de6cf80828ef9cad46060ab0
jasonsin88 <jason.c.sin@gmail.com>
2022-11-07 11:52:48 -0700

    building models

---
+++ models/Users.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thoughts');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectID,
+                ref: 'Thoughts',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectID,
+                ref: 'Users'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
+
+const Users = model('Users', userSchema);
+
+module.exports = Users;
